1975035e72a2c8cb22249a3d38539e40de760e51
adamj <a.j.jones@sky.com>
2023-05-08 18:57:31 +0100

    - Refactored plotting to separate the creation of the figure and the conversion to an image.
    - Added styling to the output of the figure to be more in line with Escape From Tarkov.
    
    - Added bender.regular.ttf to styling directory
    
    - Updated requirements.txt

--- utils/plotting.py
+++ utils/plotting.py
@@ -4,7 +4,43 @@ from typing import List
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from matplotlib import pyplot as plt
+from matplotlib import font_manager as fm
+from scipy.signal import find_peaks
+
+
+# ---------------- Set the style: ----------------
+SCATTER_COLOUR = "#31c974"
+GRID_COLOR = "#333333"
+BORDER_COLOUR = "#9a8866"
+BACKGROUND_COLOUR = "black"
+TEXT_COLOUR = "white"
+FONT_PATH = 'styling/bender.regular.ttf'
+
+FONT = fm.FontProperties(fname=FONT_PATH)
+fm.fontManager.addfont(FONT_PATH)
+
+sns.set_style("darkgrid", {
+    # Axes:
+    "axes.facecolor": BACKGROUND_COLOUR,
+    "axes.labelcolor": BORDER_COLOUR,
+    "axes.edgecolor": BORDER_COLOUR,
+
+    # Figure:
+    "figure.facecolor": BACKGROUND_COLOUR,
+
+    # Grid:
+    "grid.color": GRID_COLOR,
+    "grid.alpha": 0.5,
+
+    # Ticks:
+    "xtick.color": BORDER_COLOUR,
+    "ytick.color": BORDER_COLOUR,
+
+    "font.family": FONT.get_name(),
+
+})
 
 
 def get_recoil_pattern(file: str) -> pd.DataFrame:
@@ -37,12 +73,11 @@ def get_recoil_pattern(file: str) -> pd.DataFrame:
     df["combined_y"] = df.reticle_y + df.camera_y
     df["combined_y_move"] = df.combined_y - df.combined_y.shift(1)
     df["combined_y_color"] = df.combined_y_move.apply(lambda x: "red" if x > 0 else "blue")
-    df = df[["filename", "combined_x", "combined_y", "combined_y_color", "camera_x", "camera_y"]]
 
     return df
 
 
-def create_plots(files: List[str | Path]) -> np.ndarray:
+def create_plots(files: List[str | Path]) -> plt.Figure:
     """
     Create the plots for the recoil pattern and movement.
 
@@ -50,55 +85,82 @@ def create_plots(files: List[str | Path]) -> np.ndarray:
     @return:        Image of the plots.
     """
 
-    plots = 1
     width = len(files) * 3
     fig, axs = plt.subplots(figsize=(width, 8))
 
     df = pd.concat([get_recoil_pattern(file) for file in files])
 
     offset = 0
-    labels = []
-    for file in df.filename.unique():
-        # Test camera differences:
-        df_test = df[df.filename == file]
-        x_difference = df_test.camera_x.iloc[0] - df_test.camera_x.iloc[-1]
-        y_difference = df_test.camera_y.iloc[0] - df_test.camera_y.iloc[-1]
+    for filename, df_plot in df.groupby("filename"):
+        # Test camera differences
+        x_difference = df_plot.camera_x.iloc[0] - df_plot.camera_x.iloc[-1]
+        y_difference = df_plot.camera_y.iloc[0] - df_plot.camera_y.iloc[-1]
         euc_distance = np.sqrt(x_difference ** 2 + y_difference ** 2)
 
         # Add label for camera movement:
+        axs.text(
+            x=offset,
+            y=df_plot.combined_y.max() + 30,
+            s=filename.replace("_", "\n"),
+            color=TEXT_COLOUR,
+            horizontalalignment="center"
+        )
         if euc_distance > 25:
-            label = {
-                "x": offset,
-                "y": df[df.filename == file].combined_y.max() + 15,
-                "s": f"CAMERA MISALIGNED {euc_distance:.0f}px",
-                "color": "red",
-                "horizontalalignment": "center"
-            }
-            labels.append(label)
+            axs.text(
+                x=offset,
+                y=df_plot.combined_y.max() + 15,
+                s=f"CAMERA MISALIGNED {euc_distance:.0f}px",
+                color="red",
+                horizontalalignment="center"
+            )
 
-        # Create labels for plot:
-        label = {
-            "x": offset,
-            "y": df[df.filename == file].combined_y.max() + 30,
-            "s": file.replace("_", "\n"),
-            "horizontalalignment": "center"
-        }
-        labels.append(label)
-
-        # Create offset for the next scatter:
-        additional_offset = df[df.filename == file].combined_x.max() + 100
-        df.loc[df.filename == file, "combined_x"] = df[df.filename == file].combined_x + offset
+        # Update offset for the next scatter
+        additional_offset = df_plot.combined_x.max() + 100
+        df_plot["combined_x"] = df_plot["combined_x"] + offset
         offset += additional_offset
 
-    # Plot labels:
-    for label in labels:
-        axs.text(**label)
+        axs.plot(
+            df_plot.combined_x,
+            df_plot.combined_y,
+            color=SCATTER_COLOUR,
+            linewidth=1,
+            alpha=0.5,
+            zorder=1
+        )
 
-    # Spray pattern plotted:
-    axs.scatter(df.combined_x, df.combined_y, c=df.combined_y_color)
+        # Find the individual shots and scatter plot them:
+        diff_arr = (df_plot.combined_y - df_plot.combined_y.shift(1))
+        peaks, _ = find_peaks(diff_arr, height=diff_arr.mean() + 1, distance=3)
+        shots = df_plot.iloc[peaks - 1]
+
+        # Spray pattern plotted
+        axs.scatter(
+            shots.combined_x,
+            shots.combined_y,
+            color=SCATTER_COLOUR,
+            linewidth=1,
+            alpha=1,
+            zorder=1,
+            marker="x"
+        )
+
     axs.set_title(f"Combined reticle and camera recoil")
     axs.axis('equal')
     axs.set_ylim(df.combined_y.min() - 25, df.combined_y.max() + 75)
+
+    # Set title color
+    axs.title.set_color(BORDER_COLOUR)
+
+    return fig
+
+
+def fig_to_img(fig: plt.Figure) -> np.ndarray:
+    """
+    Convert a matplotlib figure to a numpy array with RGBA channels and return it.
+
+    @param fig: matplotlib figure to convert.
+    @return:
+    """
 
     with io.BytesIO() as buff:
         fig.savefig(buff, format='raw')
